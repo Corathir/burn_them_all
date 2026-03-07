519d7d9ee73665673606a9f072ceb8c1230d25ac
Василин Юрий <yvasilin@gmail.com>
2026-03-08 00:29:30 +0300

    spell ui clean code

--- scripts/ui/spell_button.gd
+++ scripts/ui/spell_button.gd
@@ -4,21 +4,25 @@ class_name SpellButton
 
 var spell_name: String
 var heat_cost: int
-var spell_resourse: SpellResource
+var spell_resource: SpellResource
 
 signal pressed_spell(spell_res)
 
 @onready var overlay = $Overlay
 @onready var click_area: Button = $ClickArea
+@onready var name_label = $Column/NameLabel
+@onready var cost_label: Button = $Column/CostLabel
 
 func init(spell: SpellResource):
+    name_label.text = spell_name
+    cost_label.text = str(spell.heat_cost)
     spell_name = spell.spell_name
     heat_cost = spell.heat_cost
+    spell_resource = spell
     click_area.pressed.connect(_on_click)
 
 func update_disabled_state(current_heat: int):
-    overlay.visible = current_heat > heat_cost
+    overlay.visible = current_heat < heat_cost
 
 func _on_click():
-    pressed_spell.emit(spell_resourse)
-    pass
+    pressed_spell.emit(spell_resource)
--- scripts/ui/spell_panel.gd
+++ scripts/ui/spell_panel.gd
@@ -2,11 +2,10 @@ extends Control
 
 class_name SpellPanel
 
-const spell_button_scene: String = "res://scenes/ui/spell_button.tscn"
-
+@onready var button_container: GridContainer = $GridContainer
 @onready var end_turn_button: Button = $EndTurn
 
-var buttons = Array[SpellButton]
+var buttons: Array[SpellButton] = []
 
 func _ready() -> void:
     EventBus.heat_changed.connect(_heat_changed)
@@ -14,10 +13,10 @@ func _ready() -> void:
 
 func load_spells(spells: Array[SpellResource]):
     for spell in spells:
-        var spell_button = preload(self.spell_button_scene).instantiate()
+        var spell_button = preload("res://scenes/ui/spell_button.tscn").instantiate()
         spell_button.init(spell)
-        self.add_child(spell_button)
-        buttons.add(spell_button)
+        button_container.add_child(spell_button)
+        buttons.append(spell_button)
         spell_button.pressed_spell.connect(_on_spell_click)
         
 func _on_spell_click(spell: SpellResource):
